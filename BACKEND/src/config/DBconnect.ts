3fb1357c062ee1990413fd3bacd74772aa95af6c
yashpal singh <yashpalsingh9rx@outlook.com>
2024-01-29 01:59:55 +0530

    edit .env file

--- BACKEND/src/config/DBconnect.ts
+++ BACKEND/src/config/DBconnect.ts
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
+import {config} from "./config";
 
 const dbConnect = async ():Promise<void> => {
     try {
         await mongoose.connect("mongodb://localhost:27017/todo");
-        // await mongoose.connect("mongodb+srv://yashpal9rx:D560075WIN9SATLAS@mernapp.hvyowyg.mongodb.net/TODO?retryWrites=true&w=majority");
+        // await mongoose.connect(config.MONGO_URI);
     } catch (err) {
         console.error("Error in mongodb initial connection");
     }
--- BACKEND/src/config/config.ts
+++ BACKEND/src/config/config.ts
@@ -7,6 +7,7 @@ interface EnvConfig {
     ACCESS_TOKEN_SECRET: string;
     REFRESH_TOKEN_SECRET: string;
     GEMINI_AI:string;
+    MONGO_URI:string;
 }
 
 cfg({ path: path.join(__dirname, '../../.env') });
@@ -14,7 +15,8 @@ cfg({ path: path.join(__dirname, '../../.env') });
 const envVarsSchema = z.object({
     ACCESS_TOKEN_SECRET: z.string().min(1).refine((data: string):boolean => data.trim() !== '', { message: 'Provide access token secret'}),
     REFRESH_TOKEN_SECRET: z.string().min(1).refine((data: string):boolean => data.trim() !== '', { message: 'Provide refresh token secret'}),
-    GEMINI_AI:z.string().min(1).refine((data:string):boolean => data.trim()!=='',{message:'Provide gemini ai key'})
+    GEMINI_AI:z.string().min(1).refine((data:string):boolean => data.trim()!=='',{message:'Provide gemini ai key'}),
+    MONGO_URI:z.string().min(1).refine((data:string):boolean=>data.trim()!=='',{message:'Provide mongo uri'})
 });
 
 export const validateEnv = (): EnvConfig => {
@@ -24,6 +26,7 @@ export const validateEnv = (): EnvConfig => {
             ACCESS_TOKEN_SECRET: envVars.ACCESS_TOKEN_SECRET,
             REFRESH_TOKEN_SECRET: envVars.REFRESH_TOKEN_SECRET,
             GEMINI_AI:envVars.GEMINI_AI,
+            MONGO_URI:envVars.MONGO_URI
         };
     } catch (error:any) {
         throw new Error(error.message);
